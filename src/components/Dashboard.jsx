0f99a78b83abfe5c867b6b81df60a1b6cb61cecd
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2024-03-08 16:33:02 +0100

    Swiping page added

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,38 +1,113 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useMemo, useRef, useEffect } from 'react'
+import TinderCard from 'react-tinder-card';
+import './Dashboard.css'
+import axios from "axios";
 
-const Dashboard = () => {
+function Dashboard () {
+
     const [images, setImages] = useState([]);
-
+  
     useEffect(() => {
-        axios.get('http://127.0.0.1:8000/api/images')
-            .then(response => {
-                const modifiedImages = response.data.images.map(image =>
-                    image.replace('public', 'storage')
-                );
-                setImages(modifiedImages);
-            })
-            .catch(error => {
-                console.error('Error fetching images:', error);
-            });
+      axios.get('http://127.0.0.1:8000/api/images')
+        .then(response => {
+          const modifiedImages = response.data.images.map(image =>
+            image.replace('public', 'http://127.0.0.1:8000/storage')
+          );
+          console.log(modifiedImages)
+          setImages(modifiedImages);
+        })
+        .catch(error => {
+          console.error('Error fetching images:', error);
+        });
     }, []);
+    
+  const [currentIndex, setCurrentIndex] = useState(images.length - 1)
+  const [lastDirection, setLastDirection] = useState()
+  // used for outOfFrame closure
+  const currentIndexRef = useRef(currentIndex)
 
-    return (<>
-        <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 to-purple-300">
-            <div>
-                <h1 className="text-2xl font-bold mb-4">Your Dashboard</h1>
-                {images.map((image, index) => (
-                    <img
-                        key={index}
-                        src={`http://127.0.0.1:8000/${image}`}
-                        alt={`Image ${index}`}
-                        className="w-full object-cover h-64 rounded-md"
-                    />
-                ))}
+  const childRefs = useMemo(
+    () =>
+      Array(images.length)
+        .fill(0)
+        .map((i) => React.createRef()),
+    []
+  )
+
+  const updateCurrentIndex = (val) => {
+    setCurrentIndex(val)
+    currentIndexRef.current = val
+  }
+
+  const canGoBack = currentIndex < images.length - 1
+
+  const canSwipe = currentIndex >= 0
+
+  // set last direction and decrease current index
+  const swiped = (direction, nameToDelete, index) => {
+    setLastDirection(direction)
+    updateCurrentIndex(index - 1)
+  }
+
+  const outOfFrame = (name, idx) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current)
+    // handle the case in which go back is pressed before card goes outOfFrame
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard()
+    // TODO: when quickly swipe and restore multiple times the same card,
+    // it happens multiple outOfFrame events are queued and the card disappear
+    // during latest swipes. Only the last outOfFrame event should be considered valid
+  }
+
+  const swipe = async (dir) => {
+    if (canSwipe && currentIndex < images.length) {
+      await childRefs[currentIndex].current.swipe(dir) // Swipe the card!
+    }
+  }
+
+  // increase current index and show card
+  const goBack = async () => {
+    if (!canGoBack) return
+    const newIndex = currentIndex + 1
+    updateCurrentIndex(newIndex)
+    await childRefs[newIndex].current.restoreCard()
+  }
+
+  return (
+    <div>
+      <h1>React Tinder Card</h1>
+      <div className='cardContainer'>
+        {images.map((imageUrl, index) => (
+          <TinderCard
+            ref={childRefs[index]}
+            className='swipe'
+            key={imageUrl}
+            onSwipe={(dir) => swiped(dir, imageUrl, index)}
+            onCardLeftScreen={() => outOfFrame(imageUrl, index)}
+          >
+            <div
+              style={{ backgroundImage: `url(${imageUrl})` }}
+              className='card'
+            >
             </div>
-        </div>
-    </>
-    );
-};
+          </TinderCard>
+        ))}
+      </div>
+      <div className='buttons'>
+        <button style={{ backgroundColor: !canSwipe && '#c3c4d3' }} onClick={() => swipe('left')}>Swipe left!</button>
+        <button style={{ backgroundColor: !canGoBack && '#c3c4d3' }} onClick={() => goBack()}>Undo swipe!</button>
+        <button style={{ backgroundColor: !canSwipe && '#c3c4d3' }} onClick={() => swipe('right')}>Swipe right!</button>
+      </div>
+      {lastDirection ? (
+        <h2 key={lastDirection} className='infoText'>
+          You swiped {lastDirection}
+        </h2>
+      ) : (
+        <h2 className='infoText'>
+          Swipe a card or press a button to get Restore Card button visible!
+        </h2>
+      )}
+    </div>
+  )
+}
 
-export default Dashboard;
+export default Dashboard
