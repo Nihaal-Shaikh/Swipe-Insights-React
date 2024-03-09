55d334010c97f19dec3086d5d1e104f6d07dbb32
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2024-03-09 16:20:39 +0100

    Up and down swipe functionality added

--- src/components/ImageSwiper.jsx
+++ src/components/ImageSwiper.jsx
@@ -4,17 +4,17 @@ import './ImageSwiper.css'
 import axios from "axios";
 import { useAuth } from '../store/AuthContext';
 import FinalModal from './FinalModal';
+import FinalLoader from './FinalLoader';
 
 export default function ImageSwiper() {
 
   const { userToken, tokenableId } = useAuth();
   const dialog = useRef();
 
-  console.log(tokenableId);
-
   const [images, setImages] = useState([]);
   const [left, setLeft] = useState('Left');
   const [right, setRight] = useState('Right');
+  const upOrDown = 'Unsure';
   const [swipeData, setSwipeData] = useState({});
   const [countdown, setCountdown] = useState(5);
 
@@ -65,7 +65,7 @@ export default function ImageSwiper() {
     // Update swipe data
     setSwipeData((prevSwipeData) => ({
       ...prevSwipeData,
-      [nameToDelete]: direction === 'left' ? left : right,
+      [nameToDelete]: direction === 'left' ? left : (direction === 'right' ? right : upOrDown),
     }));
   }
 
@@ -105,7 +105,8 @@ export default function ImageSwiper() {
   useEffect(() => {
     let countdownInterval;
 
-    if (swipeDataLength === 5) {
+
+    if (swipeDataLength === images.length && images.length !== 0) {
       countdownInterval = setInterval(() => {
         setCountdown((prevCountdown) => (prevCountdown > 0 ? prevCountdown - 1 : 0));
       }, 1000);
@@ -119,7 +120,6 @@ export default function ImageSwiper() {
   
   useEffect(() => {
     if(countdown === 0){
-      console.log(swipeData)
       // Make API call to send swipeData to your Laravel endpoint
       axios.post('http://127.0.0.1:8000/api/submit-swipe-data', {
         swipeData: swipeData,
@@ -156,19 +156,14 @@ export default function ImageSwiper() {
             </div>
           </TinderCard>
         ))}
-        {swipeDataLength === 5 &&
+        {swipeDataLength === images.length && (
           <>
             <FinalModal ref={dialog} />
             <p>Your answers will be submitted in</p>
             <span className="text-4xl font-bold mr-2">{countdown}</span>
-            <div role="status">
-              <svg aria-hidden="true" class="inline w-50 h-50 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-              </svg>
-            </div>
+            <FinalLoader />
           </>
-        }
+        )}
       </div>
       <div className='buttons'>
         <button style={{ backgroundColor: !canSwipe && '#c3c4d3' }} onClick={() => swipe('left')}>{left}</button>
@@ -177,7 +172,7 @@ export default function ImageSwiper() {
       </div>
       {lastDirection ? (
         <h2 key={lastDirection} className='infoText'>
-          Marked as: {lastDirection === 'left' ? left : right}
+          Marked as: {lastDirection === 'left' ? left : (lastDirection === 'right' ? right : upOrDown)}
         </h2>
       ) : (
         <h2 className='infoText'>
