50d47a9807767f6189dc526db9b8295d97ccd53a
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2024-03-10 15:00:38 +0100

    Sidebar items modified

--- src/components/Admin/lib/constants/navigation.jsx
+++ src/components/Admin/lib/constants/navigation.jsx
@@ -23,42 +23,15 @@ export const DASHBOARD_SIDEBAR_LINKS = [
 		icon: <HiOutlineCube />
 	},
 	{
-		key: 'orders',
-		label: 'Orders',
-		path: '/orders',
+		key: 'swipe-options',
+		label: 'Swipe Options',
+		path: '/web-admin/swipe-options',
 		icon: <HiOutlineShoppingCart />
 	},
 	{
-		key: 'customers',
-		label: 'Customers',
-		path: '/customers',
+		key: 'usrs',
+		label: 'Users',
+		path: '/web-admin/users',
 		icon: <HiOutlineUsers />
-	},
-	{
-		key: 'transactions',
-		label: 'Transactions',
-		path: '/transactions',
-		icon: <HiOutlineDocumentText />
-	},
-	{
-		key: 'messages',
-		label: 'Messages',
-		path: '/messages',
-		icon: <HiOutlineAnnotation />
 	}
-]
-
-export const DASHBOARD_SIDEBAR_BOTTOM_LINKS = [
-	{
-		key: 'settings',
-		label: 'Settings',
-		path: '/settings',
-		icon: <HiOutlineCog />
-	},
-	{
-		key: 'support',
-		label: 'Help & Support',
-		path: '/support',
-		icon: <HiOutlineQuestionMarkCircle />
-	}
-]
+];
