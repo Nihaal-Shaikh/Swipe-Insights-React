16881d6d1cf1ed165f31bd3935a147eaae7bec20
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2024-03-10 12:40:34 +0100

    Working on admin

--- src/components/Admin/Layout.jsx
+++ src/components/Admin/Layout.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 export default function Layout() {
 
     return (
-        <div>
-            <div className="bg-sky-200">Sidebar</div>
-            <div className="bg-teal-200">Header</div>
+        <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
+            <Sidebar />
+            <div className="p-4">
+            {/* <div className="bg-teal-200">Header</div> */}
             <div>{<Outlet />}</div>
-            <p>Footer</p>
+            </div>
+            {/* <p>Footer</p> */}
         </div>
     );
 }
--- src/components/Admin/Sidebar.jsx
+++ src/components/Admin/Sidebar.jsx
@@ -1,22 +1,33 @@
-// Sidebar.jsx
-
 import React from 'react';
+import { FcLineChart } from "react-icons/fc";
+import { DASHBOARD_SIDEBAR_LINKS } from './lib/constants/navigation';
+import { Link } from "react-router-dom";
 
-const Sidebar = () => {
+const linkClasses = 'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base';
+
+export default function Sidebar() {
   return (
-    <div className="bg-gray-800 text-white h-screen w-1/5 p-4">
-      <h2 className="text-xl font-bold mb-4">Dashboard</h2>
-      <ul>
-        <li className="mb-2">
-          <a href="#" className="text-blue-300 hover:text-blue-500">Dashboard</a>
-        </li>
-        <li className="mb-2">
-          <a href="#" className="text-blue-300 hover:text-blue-500">Analytics</a>
-        </li>
-        {/* Add more menu items as needed */}
-      </ul>
+    <div className='bg-neutral-900 w-60 p-3 flex flex-col text-white'>
+      <div className='flex items-center gap-2 px-1 py-3'>
+        <FcLineChart fontSize={24} />
+        <span className='text-neutral-100 text-lg'>Swipe Insights</span>
+      </div>
+      <div className='py-8 flex flex-1 flex-col gap-0.5'>
+        {DASHBOARD_SIDEBAR_LINKS.map((item) => (
+          <SidebarLink key={item.key} item={item} />
+        ))}
+      </div>
+      <div>Bottom part</div>
     </div>
   );
 };
 
-export default Sidebar;
+function SidebarLink({ item }) {
+
+  return (
+    <Link to={item.path} className={linkClasses}>
+      <span className='text-xl'>{item.icon}</span>
+      {item.label}
+    </Link>
+  );
+}
