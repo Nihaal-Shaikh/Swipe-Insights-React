cc5ac50203d07bd66790db80880637784b543002
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2024-03-10 12:59:18 +0100

    Working on admin

--- src/components/Admin/Layout.jsx
+++ src/components/Admin/Layout.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import Header from "./Header";
 
 export default function Layout() {
 
     return (
         <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
             <Sidebar />
-            <div className="p-4">
-            {/* <div className="bg-teal-200">Header</div> */}
-            <div>{<Outlet />}</div>
+            <div className="flex flex-col flex-1">
+                <Header />
+                <div className="flex-1 p-4 min-h-0 overflow-auto">
+                    {<Outlet />}
+                </div>
             </div>
             {/* <p>Footer</p> */}
         </div>
