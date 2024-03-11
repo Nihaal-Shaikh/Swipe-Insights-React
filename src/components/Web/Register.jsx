2a0ceaa3a846b9f38ee622e10978ef3aee01bc4c
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2024-03-11 11:57:44 +0100

    Main page design fixes

--- src/components/Web/Register.jsx
+++ src/components/Web/Register.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 export default function Register({ children }) {
+
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
 
   return (
     <>
@@ -6,6 +12,20 @@ export default function Register({ children }) {
         <input
             type="email"
             placeholder="Email"
+            className="w-full p-3 rounded-md border border-gray-300 focus:outline-none focus:border-teal-500"
+        />
+        <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full p-3 rounded-md border border-gray-300 focus:outline-none focus:border-teal-500"
+        />
+        <input
+            type="confirm-password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
             className="w-full p-3 rounded-md border border-gray-300 focus:outline-none focus:border-teal-500"
         />
         <button
